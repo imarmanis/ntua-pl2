201f4c471376db9aa2b0592af5f666eefe8bf53d
Iaswn Marmanis <j.ason_96@hotmail.com>
2018-12-30 23:39:12 +0200

    Threaded gc

--- Ex_5/gc.c
+++ Ex_5/gc.c
@@ -110,148 +110,209 @@ int run()
     clock_t start = clock();
     uint8_t *pc = prg;
     sp = stack;
+    static void *label_tab[51] = {
+        &&halt_label,
+        &&jump_label,
+        &&jnz_label,
+        &&dup_label,
+        &&swap_label,
+        &&drop_label,
+        &&push4_label,
+        &&push2_label,
+        &&push1_label,
+        &&add_label,
+        &&sub_label,
+        &&mul_label,
+        &&div_label,
+        &&mod_label,
+        &&eq_label,
+        &&ne_label,
+        &&lt_label,
+        &&gt_label,
+        &&le_label,
+        &&ge_label,
+        &&not_label,
+        &&and_label,
+        &&or_label,
+        &&input_label,
+        &&output_label
+    };
+    label_tab[CLOCK] = &&clock_label;
+    label_tab[CONS] = &&cons_label;
+    label_tab[HD] = &&hd_label;
+    label_tab[TL] = &&tl_label;
+#define NEXT_INSTR goto *(void *)(label_tab[*pc])
     for(;;) {
         switch(pc[0]) {
             case HALT:
+halt_label:
                 return 0;
             case JUMP:
+jump_label:
                 pc = &prg[get_2b(pc + 1)];
-                break;
+                NEXT_INSTR;
             case JNZ:
+jnz_label:
                 pc = (deci(*--sp) != 0) ? &prg[get_2b(pc + 1)] : pc + 3;
-                break;
+                NEXT_INSTR;
             case DUP:
+dup_label:
                 {
                     int8_t ind = -pc[1];
                     int32_t d = sp[ind-1];
                     *sp++ = d;
                     pc += 2;
-                    break;
+                    NEXT_INSTR;
                 }
             case SWAP:
+swap_label:
                 {
                     int8_t ind = -pc[1];
                     int32_t d = sp[ind-1];
                     sp[ind-1] = sp[-1];
                     sp[-1] = d;
                     pc += 2;
-                    break;
+                    NEXT_INSTR;
                 }
             case DROP:
+drop_label:
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case PUSH4:
+push4_label:
                 *sp++ = enci(get_4b(++pc));
                 pc += 4;
-                break;
+                NEXT_INSTR;
             case PUSH2:
+push2_label:
                 *sp++ =  enci(get_2b(++pc));
                 pc += 2;
-                break;
+                NEXT_INSTR;
             case PUSH1:
+push1_label:
                 *sp++ = enci(*++pc);
                 pc++;
-                break;
+                NEXT_INSTR;
             case ADD:
+add_label:
                 sp[-2] = enci(deci(sp[-2]) + deci(sp[-1]));
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case SUB:
+sub_label:
                 sp[-2] = enci(deci(sp[-2]) - deci(sp[-1]));
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case MUL:
+mul_label:
                 sp[-2] = enci(deci(sp[-2]) * deci(sp[-1]));
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case DIV:
+div_label:
                 sp[-2] = enci(deci(sp[-2]) / deci(sp[-1]));
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case MOD:
+mod_label:
                 sp[-2] = enci(deci(sp[-2]) % deci(sp[-1]));
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case EQ:
+eq_label:
                 sp[-2] = (deci(sp[-2]) == deci(sp[-1])) ? enci(1) : enci(0);
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case NE:
+ne_label:
                 sp[-2] = (deci(sp[-2]) != deci(sp[-1])) ? enci(1) : enci(0);
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case LT:
+lt_label:
                 sp[-2] = (deci(sp[-2]) < deci(sp[-1])) ? enci(1) : enci(0);
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case GT:
+gt_label:
                 sp[-2] = (deci(sp[-2]) > deci(sp[-1])) ? enci(1) : enci(0);
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case LE:
+le_label:
                 sp[-2] = (deci(sp[-2]) <= deci(sp[-1])) ? enci(1) : enci(0);
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case GE:
+ge_label:
                 sp[-2] = (deci(sp[-2]) >= deci(sp[-1])) ? enci(1) : enci(0);
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case NOT:
+not_label:
                 sp[-1] = (deci(sp[-1]) == 0) ? enci(1) : enci(0);
-                break;
+                NEXT_INSTR;
             case AND:
+and_label:
                 sp[-2] = (deci(sp[-2] != 0) && deci(sp[-1] != 0)) ? enci(1) : enci(0);
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case OR:
+or_label:
                 sp[-2] = (deci(sp[-2] != 0) || deci(sp[-1] != 0)) ? enci(1) : enci(0);
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case INPUT:
+input_label:
                 *sp++ = enci(getchar());
                 pc++;
-                break;
+                NEXT_INSTR;
             case OUTPUT:
+output_label:
                 putchar(deci(*--sp));
                 pc++;
-                break;
+                NEXT_INSTR;
             case CONS:
+cons_label:
                 {
                     cell_t *c = alloc(sp);
                     c->tl = *--sp;
                     c->hd = *--sp;
                     *sp++ = encp(c - heap.start);
                     pc++;
-                    break;
+                    NEXT_INSTR;
                 }
             case HD:
+hd_label:
                 sp[-1] = heap.start[decp(sp[-1])].hd;
                 pc++;
-                break;
+                NEXT_INSTR;
             case TL:
+tl_label:
                 sp[-1] = heap.start[decp(sp[-1])].tl;
                 pc++;
-                break;
+                NEXT_INSTR;
             case CLOCK:
+clock_label:
                 {
                     clock_t end = clock();
                     printf("%0.6lf\n", (double)(end - start)/CLOCKS_PER_SEC);
                     pc++;
-                    break;
+                    NEXT_INSTR;
                 }
         }
     }
