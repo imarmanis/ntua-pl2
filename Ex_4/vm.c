58aab3f35b97d1d26299aaae617d9d87e6a7d811
Iaswn Marmanis <j.ason_96@hotmail.com>
2018-12-30 23:20:50 +0200

    Threaded vm

--- Ex_4/vm.c
+++ Ex_4/vm.c
@@ -33,132 +33,187 @@ int run(byte *prg, word *stack)
     clock_t start = clock();
     byte *pc = prg;
     word *sp = stack;
+    static void *label_tab[33] = {
+        &&halt_label,
+        &&jump_label,
+        &&jnz_label,
+        &&dup_label,
+        &&swap_label,
+        &&drop_label,
+        &&push4_label,
+        &&push2_label,
+        &&push1_label,
+        &&add_label,
+        &&sub_label,
+        &&mul_label,
+        &&div_label,
+        &&mod_label,
+        &&eq_label,
+        &&ne_label,
+        &&lt_label,
+        &&gt_label,
+        &&le_label,
+        &&ge_label,
+        &&not_label,
+        &&and_label,
+        &&or_label,
+        &&input_label,
+        &&output_label
+    };
+    label_tab[CLOCK] = &&clock_label;
+#define NEXT_INSTR goto *(void *)(label_tab[*pc])
     for(;;) {
         byte opcode = pc[0];
         switch(opcode) {
             case HALT:
+halt_label:
                 return 0;
             case JUMP:
+jump_label:
                 pc = &prg[get_2b(pc + 1)];
-                break;
+                NEXT_INSTR;
             case JNZ:
+jnz_label:
                 pc = (*--sp != 0) ? &prg[get_2b(pc + 1)] : pc + 3;
-                break;
+                NEXT_INSTR;
             case DUP:
+dup_label:
                 {
                     int8_t ind = -pc[1];
                     word d = sp[ind-1];
                     *sp++ = d;
                     pc += 2;
-                    break;
+                    NEXT_INSTR;
                 }
             case SWAP:
+swap_label:
                 {
                     int8_t ind = -pc[1];
                     word d = sp[ind-1];
                     sp[ind-1] = sp[-1];
                     sp[-1] = d;
                     pc += 2;
-                    break;
+                    NEXT_INSTR;
                 }
             case DROP:
+drop_label:
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case PUSH4:
+push4_label:
                 *sp++ = get_4b(++pc);
                 pc += 4;
-                break;
+                NEXT_INSTR;
             case PUSH2:
+push2_label:
                 *sp++ =  get_2b(++pc);
                 pc += 2;
-                break;
+                NEXT_INSTR;
             case PUSH1:
+push1_label:
                 *sp++ = *++pc;
                 pc++;
-                break;
+                NEXT_INSTR;
             case ADD:
+add_label:
                 sp[-2] += sp[-1];
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case SUB:
+sub_label:
                 sp[-2] -= sp[-1];
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case MUL:
+mul_label:
                 sp[-2] *= sp[-1];
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case DIV:
+div_label:
                 sp[-2] /= sp[-1];
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case MOD:
+mod_label:
                 sp[-2] %= sp[-1];
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case EQ:
+eq_label:
                 sp[-2] = (sp[-2] == sp[-1]) ? 1 : 0;
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case NE:
+ne_label:
                 sp[-2] = (sp[-2] != sp[-1]) ? 1 : 0;
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case LT:
+lt_label:
                 sp[-2] = (sp[-2] < sp[-1]) ? 1 : 0;
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case GT:
+gt_label:
                 sp[-2] = (sp[-2] > sp[-1]) ? 1 : 0;
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case LE:
+le_label:
                 sp[-2] = (sp[-2] <= sp[-1]) ? 1 : 0;
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case GE:
+ge_label:
                 sp[-2] = (sp[-2] >= sp[-1]) ? 1 : 0;
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case NOT:
+not_label:
                 sp[-1] = (sp[-1] == 0) ? 1 : 0;
-                break;
+                NEXT_INSTR;
             case AND:
+and_label:
                 sp[-2] = (sp[-2] != 0 && sp[-1] != 0) ? 1 : 0;
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case OR:
+or_label:
                 sp[-2] = (sp[-2] != 0 || sp[-1] != 0) ? 1 : 0;
                 sp--;
                 pc++;
-                break;
+                NEXT_INSTR;
             case INPUT:
+input_label:
                 *sp++ = getchar();
                 pc++;
-                break;
+                NEXT_INSTR;
             case OUTPUT:
+output_label:
                 putchar(*--sp);
                 pc++;
-                break;
+                NEXT_INSTR;
             case CLOCK:
+clock_label:
                 {
                     clock_t end = clock();
                     printf("%0.6lf\n", (double)(end - start)/CLOCKS_PER_SEC);
                     pc++;
-                    break;
+                    NEXT_INSTR;
                 }
         }
     }
